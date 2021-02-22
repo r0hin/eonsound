368ed7465971f0455b3202a34a755ab4b6fdd216
Rohin <me@r0h.in>
2021-02-22 00:52:14 -0500

    💬 Discussion Forum (1.8.5)
    - Added the chat / forum section to the discussions.
    - Trending searches now show and will go to that forum on click.
    - Cool looking header!
    - Some bugs fixed and more organized code.

--- app/js/discuss.js
+++ app/js/discuss.js
@@ -1,7 +1,14 @@
+const db = firebase.firestore()
+
 window.musicData = {}
+window.activeID = ''
 
 document.getElementById('discussionsearchbox').addEventListener("keyup", function(event) {
   if (event.keyCode === 13) { event.preventDefault(); searchDiscussion(document.getElementById('discussionsearchbox').value)}
+});
+
+document.getElementById('msgbox').addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) { event.preventDefault(); addMessage(document.getElementById('msgbox').value); document.getElementById('msgbox').value = ''}
 });
 
 
@@ -25,10 +32,26 @@ function setDefaultVolume() {
 function initDiscussions() {
   initSpotifyCode();
   setDefaultVolume();
+  getTrending()
   // Get trending searches
 }
 
+async function getTrending() {
+  var doc = await db.collection('discussions').doc('APP_TRENDING').get()
+
+  for (let i = 0; i < 3; i++) {
+    $(`#ts_${i}`).html(doc.data()[`${i}`].display)
+    $(`#ts_${i}`).get(0).onclick = () => {
+      openDiscussion(doc.data()[i].id)
+    }
+  }
+
+  $('#trending_searches').removeClass('hidden')
+
+}
+
 async function searchDiscussion(q) {
+  closeDiscussion()
   console.log('Search for discussions of ID', q)
   document.getElementById('discussionsearchbox').value = '';
   $("#theSearchResults").empty();
@@ -67,11 +90,75 @@ function confirmSongPlayed() {
   window.setTimeout(()=>{textWidth=$("#playing_track_details").width(),songActionWidth=textWidth+0+0,contentWidth=songActionWidth+68+24,playerWidth="calc(100% - "+contentWidth+"px)",$("#InjectedWidth").get(0).innerHTML=`\n      .songactions {\n        left: ${songActionWidth}px !important;\n        transition: all 1s !important;\n      }\n    \n      #player .plyr {\n        width: ${playerWidth} !important;\n        transition: all 1s !important;\n      }\n      `},1550);
 }
 
-function openDiscussion(id) {
+async function openDiscussion(id) {
+  closeDiscussion()
+
   window.setTimeout(()=>{textWidth=$("#playing_track_details").width(),songActionWidth=textWidth+0+0,contentWidth=songActionWidth+68+24,playerWidth="calc(100% - "+contentWidth+"px)",$("#InjectedWidth").get(0).innerHTML=`\n      .songactions {\n        left: ${songActionWidth}px !important;\n        transition: all 1s !important;\n      }\n    \n      #player .plyr {\n        width: ${playerWidth} !important;\n        transition: all 1s !important;\n      }\n      `},1550);
+  $("#theSearchResults").empty();
   console.log('Open discussion of ID', id)
+  window.activeID = id;
+
+  $("#theForumPanel").removeClass("hidden")
+  var songData = await goFetch('tracks/' + id)
+  var artistData = await goFetch('artists/' + songData.artists[0].id)
+  console.log(songData)
+
+  $('#header').get(0).setAttribute('style', `background-image: url('${artistData.images[0].url}')`)
+  $('#songName').html(artistData.name + ' - ' + songData.name)
+
+  var doc = await db.collection('discussions').doc(activeID).get()
+  if (!doc.exists) {
+    await db.collection('discussions').doc(activeID).set({
+      messages: [{
+        n: 'System',
+        c: 'Welcome to the discussion! You are the first one here.'
+      }]
+    })
+  }
+
+  
+  messageListener = db.collection('discussions').doc(activeID).onSnapshot((snapshot) => {
+    // Build messages
+    $('#forum').empty()
+
+    for (let i = 0; i < snapshot.data().messages.length; i++) {
+      const element = snapshot.data().messages[i];
+
+      var m = document.createElement('div')
+      m.setAttribute('class', 'msg')
+      m.innerHTML = `<b>${element.n}</b> ↠ ${element.c}`
+      document.getElementById('forum').appendChild(m)
+
+    }
+  })
+
+  // Set trending
+
+  var keyN = 'ONE'
+  var random = Math.floor(Math.random() * 3);
+
+  await db.collection("discussions").doc('APP_TRENDING').set({
+    [random]: {
+      display: `${artistData.name} - ${songData.name}`,
+      id: id
+    },
+  }, {merge: true})
+
 }
 
 function closeDiscussion() {
   console.log('Close discussion of ID')
+  $("#theForumPanel").addClass("hidden")
+  window.activeID = undefined;
+  try { messageListener() } catch (error) {
+  }
+}
+
+async function addMessage(msg) {
+  await db.collection("discussions").doc(activeID).set({
+    messages: firebase.firestore.FieldValue.arrayUnion({
+      n: cacheuser.name,
+      c: msg,
+    })
+  }, {merge: true})
 }
